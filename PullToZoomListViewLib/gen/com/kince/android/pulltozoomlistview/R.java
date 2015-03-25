dc8fa4213a75a7054a8ff5b0b6613912e67905ed
youyang <youyang@ucweb.com>
2015-03-25 15:51:43 +0800

    添加了控件属性
    可以通过控件属性，在布局文件中通过属性的形式添加listview 的head 背景图

--- PullToZoomListViewLib/gen/com/kince/android/pulltozoomlistview/R.java
+++ PullToZoomListViewLib/gen/com/kince/android/pulltozoomlistview/R.java
@@ -25,7 +25,8 @@ public final class R {
         public static int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static int ic_launcher=0x7f020000;
+        public static int aa=0x7f020000;
+        public static int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static int action_settings=0x7f080000;
